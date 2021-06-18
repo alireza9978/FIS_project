bc3dacb476955fe7d5682786df1cfa82d002ace0
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-06-18 18:46:52 +0430

    update tcp flood

--- test_scripts/TCP Flood.py
+++ test_scripts/TCP Flood.py
@@ -1,4 +1,14 @@
-import requests
+import timeit
 
-for i in range(100):
+import requests
+from joblib import Parallel, delayed
+
+
+def tcp_req(i):
     _ = requests.get(url="http://192.168.1.104:8000/login/")
+
+
+start = timeit.default_timer()
+result = Parallel(n_jobs=4)(delayed(tcp_req)(i) for i in range(10))
+stop = timeit.default_timer()
+print('Total Time: ', stop - start)

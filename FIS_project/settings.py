dd23ca74edf2a38a3abfa6e69053257b2bfccf8c
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-06-30 20:47:09 +0430

    try fix bug

--- FIS_project/settings.py
+++ FIS_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'web.BlockIp.BlockIp'
 ]
 
 ROOT_URLCONF = 'FIS_project.urls'

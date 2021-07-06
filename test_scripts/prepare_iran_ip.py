994a1aa42cba78d2e7801ae83159e3c9e0e5d6fa
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-07-06 12:16:07 +0430

    change ip format in csv

--- test_scripts/prepare_iran_ip.py
+++ test_scripts/prepare_iran_ip.py
@@ -1,8 +1,8 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 df = pd.read_csv("ir.csv", header=None)
-df["mask"] = np.array(32 - np.log2(df[2].to_numpy())).astype(np.int32)
-df["final_ip"] = df[0] + '\\' + df['mask'].astype(str)
-df = df[["final_ip"]]
-df.to_csv("iran_ip.csv", index=None, header=None)
+df["mask"] = np.array(32 - np.log2(df[2].to_numpy())).astype(np.int32).astype(str)
+df["ip"] = df[0].astype(str)
+df = df[["ip", "mask"]]
+df.to_csv("iran_ip.csv", index=None)
